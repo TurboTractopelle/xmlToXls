28308c2ce74a2764b24a01357f7e1b278ad99093
neroark <ns_ark@hotmail.com>
2019-07-25 09:54:57 +0200

    feat: promise all step2

--- src/browse.js
+++ src/browse.js
@@ -14,23 +14,40 @@ async function browse() {
   const years = await read(filesPath);
 
   return new Promise(async (res, err) => {
-    const out = await years.map(async year => {
-      const yearPath = path.join(filesPath, year);
-      const issues = await read(yearPath);
+    /*Promise.all([
+      ...[1, 2, 3].map(item => new Promise((res2, err2) => res2(item)))
+    ]).then(data => res(data));*/
 
-      return issues.map(async issue => {
-        const issuePath = path.join(yearPath, issue);
-        const files = await read(issuePath);
+    Promise.all([
+      ...years.map(async year => {
+        return new Promise(async (resYear, err2) => {
+          const yearPath = path.join(filesPath, year);
+          const issues = await read(yearPath);
 
-        return files.map(file => {
-          const filePath = path.join(issuePath, file);
-          const articlePath = path.join(filePath, `${file}.xml`);
-          return "filepath";
+          Promise.all([
+            ...issues.map(issue => {
+              return new Promise(async (resIssue, errIssue) => {
+                const issuePath = path.join(yearPath, issue);
+                const files = await read(issuePath);
+
+                resIssue(files);
+              });
+            })
+          ]).then(dataYear => resYear(dataYear));
+
+          /*issues.map(async issue => {
+            const issuePath = path.join(yearPath, issue);
+            const files = await read(issuePath);
+
+            /*return files.map(file => {
+              const filePath = path.join(issuePath, file);
+              const articlePath = path.join(filePath, `${file}.xml`);
+              res2("filepath");
+            });
+          })*/
         });
-      });
-    });
-
-    console.log(out);
+      })
+    ]).then(allYears => res(allYears));
   });
 }
 
