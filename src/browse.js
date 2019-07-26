a714d2fcc26c54409bfa8dec2959489a906c9297
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-07-26 14:43:18 +0200

    fix: affiliations

--- src/browse.js
+++ src/browse.js
@@ -4,43 +4,51 @@ const path = require("path");
 const convertFile = require("./convertFile");
 
 async function browse() {
-  /*  const articlePath =
+	/*  const articlePath =
     "C:\\Users\\neroptik\\Desktop\\var\\node\\xmlToXls\\files\\2016\\08\\tsm-2016-10\\tsm-2016-10.xml";
   console.log(await convertFile(articlePath));*/
 
-  const filesPath = path.join(__dirname, "../files");
-  const years = await read(filesPath);
+	const articlePath =
+		"C:\\Users\\gdrouen\\Desktop\\dev\\xmlToXls\\files\\2007\\02\\tsm200702p21\\tsm200702p21.xml";
 
-  return new Promise(async (res, err) => {
-    Promise.all([
-      ...years.map(async year => {
-        return new Promise(async (resYear, err2) => {
-          const yearPath = path.join(filesPath, year);
-          const issues = await read(yearPath);
+	console.log(await convertFile(articlePath));
+	//Promise.resolve(await convertFile(articlePath));
 
-          Promise.all([
-            ...issues.map(issue => {
-              return new Promise(async (resIssue, errIssue) => {
-                const issuePath = path.join(yearPath, issue);
-                const files = await read(issuePath);
+	/*
+	const filesPath = path.join(__dirname, "../files");
 
-                Promise.all([
-                  ...files.map(async file => {
-                    return new Promise(async (resFile, errFile) => {
-                      const filePath = path.join(issuePath, file);
-                      const articlePath = path.join(filePath, `${file}.xml`);
-                      const convertedFile = await convertFile(articlePath);
-                      resFile(convertedFile);
-                    });
-                  })
-                ]).then(dataFiles => resIssue(dataFiles));
-              });
-            })
-          ]).then(dataYear => resYear(dataYear));
-        });
-      })
-    ]).then(allYears => res(allYears));
+	const years = await read(filesPath);
+	return new Promise(async (res, err) => {
+		Promise.all([
+			...years.map(async year => {
+				return new Promise(async (resYear, err2) => {
+					const yearPath = path.join(filesPath, year);
+					const issues = await read(yearPath);
+
+					Promise.all([
+						...issues.map(issue => {
+							return new Promise(async (resIssue, errIssue) => {
+								const issuePath = path.join(yearPath, issue);
+								const files = await read(issuePath);
+
+								Promise.all([
+									...files.map(async file => {
+										return new Promise(async (resFile, errFile) => {
+											const filePath = path.join(issuePath, file);
+											const articlePath = path.join(filePath, `${file}.xml`);
+											const convertedFile = await convertFile(articlePath);
+											resFile(convertedFile);
+										});
+									})
+								]).then(dataFiles => resIssue(dataFiles));
+							});
+						})
+					]).then(dataYear => resYear(dataYear));
+				});
+			})
+		]).then(allYears => res(allYears));
   });
+  */
 }
 
 module.exports = browse;
--- src/convertData.js
+++ src/convertData.js
@@ -1,49 +1,146 @@
 function convertData(data) {
-  const articleType = data.article.$["article-type"];
+	const articleType = data.article.$["article-type"];
 
-  let articleRef,
-    title,
-    transTitle,
-    abstract,
-    transAbstract,
-    authors = "";
+	let articleRef,
+		title,
+		transTitle,
+		authors,
+		affiliations,
+		keywordsFr,
+		keywordsEn,
+		transAbstract = "";
 
-  if (articleType === "magazine") {
-  } else if (articleType === "research-article") {
-    articleRef = data.article.front[0]["article-meta"][0]["article-id"][1]["_"];
+	if (articleType === "magazine") {
+	} else if (articleType === "research-article") {
+		articleRef = data.article.front[0]["article-meta"][0]["article-id"][1]["_"];
 
-    title =
-      data.article.front[0]["article-meta"][0]["title-group"][0][
-        "article-title"
-      ][0]["_"];
+		title = data.article.front[0]["article-meta"][0]["title-group"][0]["article-title"][0]["_"];
 
-    /*transTitle =
-      data.article.front[0]["article-meta"][0]["title-group"][0][
-        "trans-title-group"
-      ][0]["trans-title"][0];*/
-    /*  abstract = data.article.front[0]["article-meta"][0]["abstract"][0]["p"][0];
-    transAbstract =
-      data.article.front[0]["article-meta"][0]["trans-abstract"][0]["p"][0];*/
-    /*authors = data.article.front[0]["article-meta"][0]["contrib-group"][0][
-    "contrib"
-  ]
-    .map(
-      entry => `${entry.name[0]["given-names"][0]} ${entry.name[0].surname[0]}`
-    )
-    .join(", ");*/
-    /*const keywordsFr =
-      data.article.front[0]["article-meta"][0]["kwd-group"][0]["kwd"];
-    const keywordsEn =
-      data.article.front[0]["article-meta"][0]["kwd-group"][1]["kwd"];
+		console.log(articleRef);
 
+		if (
+			data.article.front[0]["article-meta"][0]["title-group"][0] &&
+			data.article.front[0]["article-meta"][0]["title-group"][0]["trans-title-group"] &&
+			data.article.front[0]["article-meta"][0]["title-group"][0]["trans-title-group"][0][
+				"trans-title"
+			] &&
+			data.article.front[0]["article-meta"][0]["title-group"][0]["trans-title-group"][0][
+				"trans-title"
+			][0]
+		) {
+			transTitle =
+				data.article.front[0]["article-meta"][0]["title-group"][0]["trans-title-group"][0][
+					"trans-title"
+				][0];
+		}
 
+		// authors
+		if (
+			data.article.front[0] &&
+			data.article.front[0]["article-meta"] &&
+			data.article.front[0]["article-meta"][0] &&
+			data.article.front[0]["article-meta"][0]["contrib-group"] &&
+			data.article.front[0]["article-meta"][0]["contrib-group"][0] &&
+			data.article.front[0]["article-meta"][0]["contrib-group"][0]["contrib"]
+		) {
+			authors = data.article.front[0]["article-meta"][0]["contrib-group"][0]["contrib"]
+				.map(a => {
+					let given;
+					if (
+						a.name &&
+						a.name[0]["given-names"] &&
+						a.name[0]["given-names"] &&
+						a.name[0]["given-names"][0]
+					) {
+						given = a.name[0]["given-names"][0];
+					}
 
-    /*const addresses = data.article.front[0]["article-meta"][0]["aff"].map(
-    (addr, i) => addr["addr-line"][0] //addr["label"][0]:
-  );*/
-  }
+					let surname;
+					if (a.name && a.name[0] && a.name[0].surname && a.name[0].surname[0]) {
+						surname = a.name[0].surname[0];
+					}
 
-  return { articleRef, title };
+					let aff;
+					if (a.xref && a.xref && a.xref["_"]) {
+						aff = a.xref["_"];
+					}
+
+					return `${given ? given : ""}${surname ? surname : ""}${aff ? aff : ""}`;
+				})
+				.join(", ");
+		}
+
+		console.log("\n");
+		console
+			.log
+			//data.article.front[0]["article-meta"][0]["aff"][0]["addr-line"][0]["named-content"]
+			();
+		console.log("\n");
+
+		// affiliations
+		if (
+			data.article.front[0]["article-meta"] &&
+			data.article.front[0]["article-meta"][0] &&
+			data.article.front[0]["article-meta"][0]["aff"]
+		) {
+			affiliations = data.article.front[0]["article-meta"][0]["aff"]
+				.map(aff => {
+					const label = aff.label;
+					const entreprise = aff["addr-line"][0].institution;
+					const addr = aff["addr-line"][0]["named-content"];
+
+					let city, street, postcode, postbox;
+
+					const tmpCity = addr.filter(a => a["$"]["content-type"] === "city");
+					if (tmpCity[0] && tmpCity[0]["_"]) {
+						city = tmpCity[0]["_"];
+					}
+
+					const tmpStreet = addr.filter(a => a["$"]["content-type"] === "street");
+					if (tmpStreet[0] && tmpStreet[0]["_"]) {
+						street = tmpStreet[0]["_"];
+					}
+
+					const tmpPostcode = addr.filter(a => a["$"]["content-type"] === "postcode");
+					if (tmpPostcode[0] && tmpPostcode[0]["_"]) {
+						postcode = tmpPostcode[0]["_"];
+					}
+
+					const tmpPostbox = addr.filter(a => a["$"]["content-type"] === "postbox");
+					if (tmpPostbox[0] && tmpPostbox[0]["_"]) {
+						postbox = tmpPostbox[0]["_"];
+					}
+
+					return `${label}: ${entreprise},${street ? " " + street : null}${
+						postbox ? " " + postbox : null
+					}${postcode ? " " + postbox : null}${city ? " " + city : null}`;
+				})
+				.reduce((a, k) => (a = [...a, k]), [])
+				.join("\t\r");
+		}
+
+		if (
+			data.article.front[0] &&
+			data.article.front[0]["article-meta"] &&
+			data.article.front[0]["article-meta"][0]["kwd-group"] &&
+			data.article.front[0]["article-meta"][0]["kwd-group"][0] &&
+			data.article.front[0]["article-meta"][0]["kwd-group"][0]["kwd"]
+		) {
+			keywordsFr = data.article.front[0]["article-meta"][0]["kwd-group"][0]["kwd"];
+		}
+
+		if (
+			data.article.front[0] &&
+			data.article.front[0]["article-meta"] &&
+			data.article.front[0]["article-meta"][0]["kwd-group"] &&
+			data.article.front[0]["article-meta"][0]["kwd-group"][1] &&
+			data.article.front[0]["article-meta"][0]["kwd-group"][1]["kwd"]
+		) {
+			keywordsEn = data.article.front[0]["article-meta"][0]["kwd-group"][1]["kwd"];
+		}
+	}
+
+	return { articleRef, title, transTitle, authors, affiliations, keywordsFr, keywordsEn };
 }
 
 module.exports = convertData;
